b8ba1a6eb834c788320849a2964363ff4f76443b
Hycarus <mirkomarasco08@gmail.com>
2023-10-16 16:45:26 +0200

    created while width prompt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-
 const btn = document.querySelector('button');
-const inputEl = document.getElementById('data');
+const inputEl = document.getElementById('data').value;
 const resultEl = document.querySelector('.alert');
 let msg;
+let shoppingList = [];
+console.log(shoppingList);
+let stop = false;
+while(!stop){
+    let product = prompt('Cosa mettiamo nella lista della spesa?');
+    if(product === 'stop'){
+        stop = true;
+    } else{
+        shoppingList.push(product);
+    }
+};
+
